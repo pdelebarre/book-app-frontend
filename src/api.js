63bbee72b833ac482117dbc625fcafa5de6d7dfc
Philippe Delebarre <pdelebarre@gmail.com>
2024-08-02 18:00:20 +0200

    adaptiong api

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://localhost:6868/api",
 });
 
-export const getBooks = () => api.get("/books");
+export const getBooks = () => api.get("/books/all");
 export const addBook = (book) => api.post("/books", book);
 export const updateBook = (id, book) => api.put(`/books/${id}`, book);
 export const deleteBook = (id) => api.delete(`/books/${id}`);
