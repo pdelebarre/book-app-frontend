7cadf603bd8856986e14f7c5118a56123e6a71de
Philippe Delebarre <pdelebarre@gmail.com>
2024-08-21 17:42:05 +0200

    fixing websocket lib

--- src/hooks/useWebsocket.js
+++ src/hooks/useWebsocket.js
@@ -1,21 +1,36 @@
 import { useEffect } from "react";
 import SockJS from "sockjs-client";
-import { over } from "stompjs";
+import { Client } from "@stomp/stompjs";
 
 const useWebSocket = (onMessageReceived) => {
   useEffect(() => {
+    // Create a SockJS connection
     const socket = new SockJS(`${process.env.REACT_APP_API_BASE_URL}/ws`);
-    const stompClient = over(socket);
 
-    stompClient.connect({}, () => {
-      stompClient.subscribe("/topic/books", (message) => {
-        onMessageReceived(message.body);
-      });
+    // Initialize the STOMP client
+    const stompClient = new Client({
+      webSocketFactory: () => socket,
+      debug: (str) => {
+        console.log(str);
+      },
+      onConnect: () => {
+        stompClient.subscribe("/topic/books", (message) => {
+          onMessageReceived(message.body);
+        });
+      },
+      onStompError: (frame) => {
+        console.error(`Broker reported error: ${frame.headers["message"]}`);
+        console.error(`Additional details: ${frame.body}`);
+      },
     });
 
+    // Connect to the server
+    stompClient.activate();
+
+    // Cleanup on component unmount
     return () => {
       if (stompClient) {
-        stompClient.disconnect();
+        stompClient.deactivate();
       }
     };
   }, [onMessageReceived]);
